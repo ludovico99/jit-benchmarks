d33603c7fd7faa6c3c01841fe8e5660369b55736
ludovico99 <ludovico.zarr@gmail.com>
2024-01-25 18:57:15 +0100

    new test

--- run_test.sh
+++ run_test.sh
@@ -23,45 +23,43 @@ pushd ../..
 
 #-------------------------------------------------------------------- not mod --------------------------------------------------------------------
 
-# #run test
 # popd
 # python3 jit_test_fine.py $test_folder/no_module.csv
 # chown $user $test_folder/no_module.csv
 # pushd ../..
 
-#-------------------------------------------------------------------- zone sync --------------------------------------------------------------------
+# #-------------------------------------------------------------------- zone sync --------------------------------------------------------------------
 
-make EXTRA_CFLAGS=-DZONE_KERNEL_SYNC_CHECK
+# make EXTRA_CFLAGS=-DZONE_KERNEL_SYNC_CHECK
+# insmod hook.ko
+
+# python3 user/agent_zone.py &
+# AGENT_PID=$!
+
+# popd
+# python3 jit_test_fine.py $test_folder/zone_sync.csv
+# chown $user $test_folder/zone_sync.csv
+# pushd ../..
+
+# kill -9 $AGENT_PID
+# wait $AGENT_PID
+# rmmod hook.ko
+
+#---------------------------------------------------------------------- page sync --------------------------------------------------------------------
+
+make 
 insmod hook.ko
 
-python3 user/agent_zone.py &
+python3 user/agent_page.py &
 AGENT_PID=$!
 
 popd
-python3 jit_test_fine.py $test_folder/zone_sync.csv
-chown $user $test_folder/zone_sync.csv
+python3 jit_test_fine.py $test_folder/page_sync.csv
+chown $user $test_folder/page_sync.csv
 pushd ../..
 
 kill -9 $AGENT_PID
 wait $AGENT_PID
 rmmod hook.ko
-
-#---------------------------------------------------------------------- page sync --------------------------------------------------------------------
-
-# make 
-# insmod hook.ko
-
-# python3 user/agent_page.py &
-# AGENT_PID=$!
-
-# popd
-# python3 jit_test_fine.py $test_folder/page_sync.csv
-# chown $user $test_folder/page_sync.csv
-# pushd ../..
-
-# clean up
-# kill -9 $AGENT_PID
-# wait $AGENT_PID
-# rmmod hook.ko
 
 
